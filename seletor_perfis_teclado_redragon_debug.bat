c526225571693aff578af964041af60238c92bcd
gabriel <gabrielmrdp@gmail.com>
2024-08-05 23:57:04 -0300

    information to change ref.env to .env

--- seletor_perfis_teclado_redragon_debug.bat
+++ seletor_perfis_teclado_redragon_debug.bat
@@ -1,4 +1,12 @@
 @echo off
+REM Verifica se o arquivo .dot existe
+IF NOT EXIST ".env" (
+    echo O arquivo .env nao foi encontrado.
+    echo Por favor, renomeie o arquivo ref.env para .env e atualize os dados conforme necessário.
+    pause
+    exit /b
+)
+
 REM Ativa o ambiente virtual chamado "red"
 call red\Scripts\activate.bat
 
